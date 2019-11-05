85dcabadc5f4c77ff0a40a13368a5787afc45776
Kozo Oeda <kozo.oeda@gmail.com>
2019-11-05 21:56:44 +0900

    Check a todo file

--- lib/commands/c_diff.py
+++ lib/commands/c_diff.py
@@ -1,13 +1,19 @@
+import re
 from .core.git_stamp import combine_stamp
 from .core.color import color
 from .core.parser import color_diff
 from .core.git_stamp import untracked_files
+from .core.configure import load_config
+from .core.custom_types import Config
 
 
 def c_diff():
+    config: Config = load_config()
+    uuid = config["uuid"]
     for file_name, diff_text in combine_stamp(enable_time_stamp=False).items():
-        print(color(file_name, color_type="green"))
-        print(color_diff(diff_text))
+        if not re.match(f"docs\/{uuid}\/todo", file_name):
+            print(color(file_name, color_type="green"))
+            print(color_diff(diff_text))
 
     untracked_f = untracked_files()
     if untracked_f:
