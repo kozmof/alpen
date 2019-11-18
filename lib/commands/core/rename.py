2aada7d04074c6e53f5a7c438add40a08f0e8fc7
Kozo Oeda <kozo.oeda@gmail.com>
2019-11-18 21:03:51 +0900

    Add arguments

--- lib/commands/core/rename.py
+++ lib/commands/core/rename.py
@@ -3,6 +3,6 @@ from metadata import update_metadata_file
 from custom_types import Config
 
 
-def apply_rename(config: Config):
+def apply_rename(file_name, new_file_name, config: Config):
     update_tag_file("RENAME", file_name, config, new_file_name=new_file_name)
     update_metadata_file("RENAME", file_name, config, new_file_name=new_file_name)
