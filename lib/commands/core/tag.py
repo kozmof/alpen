180e778c5cd8f9d00ff324a242de24c563dcec06
Kozo Oeda <kozo.oeda@gmail.com>
2019-11-19 19:52:38 +0900

    Update

--- lib/commands/core/tag.py
+++ lib/commands/core/tag.py
@@ -41,49 +41,69 @@ def arg_check_t(action_type: str):
     if action_type == "ADD_TAG":
         if tag_name is None and new_file_name:
             raise Exception("Pass only tag_name")
-    elif action_type == "RENAME":
+    elif action_type == "REMOVE_TAG":
+        if tag_name is None and new_file_name:
+            raise Exception("Pass only tag_name")
+    elif action_type == "RENAME_FILE":
         if new_file_name is None and tag_name:
             raise Exception("Pass only new_file_name")
     else:
         raise Exception(f"No such action type: {action_type}")
 
 
+def get_tag_data(config: Config):
+    tag_dir = get_dir_path("TAG", config)
+    tag_file_path = f"{tag_dir}/{TAG_FILE}"
+
+    if os.path.isfile(tag_file_path):
+        with open(tag_file_path, "r") as fpt:
+            tag_data = json.load(fpt)
+            return tag_data
+
+
+def update_json(tag_data, config: Config):
+    tag_dir = get_dir_path("TAG", config)
+    tag_file_path = f"{tag_dir}/{TAG_FILE}"
+
+    if os.path.isfile(tag_file_path):
+        with open(tag_file_path, "w") as fpt:
+            json.dump(tag_data, fpt)
+
+
 def update_tag_file(action_type: str, file_name: str, config: Config, tag_name: Optional[str] = None, new_file_name: Optional[str] = None):
     arg_check_t(action_type)
-
     if action_type == "ADD_TAG":
-        tag_dir = get_dir_path("TAG", config)
-        tag_file_path = f"{tag_dir}/{TAG_FILE}"
+        tag_data = get_tag_data(config)
+        if tag_data:
+            if tag_name in tag_data:
+                if file_name not in tag_data[tag_name]:
+                    tag_data[tag_name].append(file_name)
+            else:
+                tag_data[tag_name] = [file_name]
 
-        if os.path.isfile(tag_file_path):
-            with open(tag_file_path, "r+") as fpt:
-                tag_data = json.load(fpt)
-
-                if tag_name in tag_data:
-                    if file_name not in tag_data[tag_name]:
-                        tag_data[tag_name].append(file_name)
-                else:
-                    tag_data[tag_name] = [file_name]
-
-                json.dump(tag_data, fpt)
+            update_json(tag_data, config)
         else:
-            tags = {}
-            tags[tag_name] = [file_name]
-            with open(tag_file_path, "w") as fpt:
-                json.dump(tags, fpt)
+            tag_data = {}
+            tag_data[tag_name] = [file_name]
+            update_json(tag_data, config)
+
+    elif action_type == "REMOVE_TAG":
+        tag_data = get_tag_data(config)
+        if tag_data:
+            if tag_name in tag_data:
+                if file_name in tag_data[tag_name]:
+                    tag_data[tag_name].remove(file_name)
+                    update_json(tag_data, config)
 
     elif action_type == "RENAME_FILE":
         tag_names = f2t(file_name, config)
         if tag_names:
-            tag_dir = get_dir_path("TAG", config)
-            tag_file_path = f"{tag_dir}/{TAG_FILE}"
-            if os.path.isfile(tag_file_path):
-                with open(tag_file_path, "r+") as fpt:
-                    tag_data = json.load(fpt)
-                    for tag_name in tag_names:
-                        tag_data[tag_name].remove(file_name)
-                        tag_data[tag_name].append(new_file_name)
+            tag_data = get_tag_data(config)
+            if tag_data:
+                for tag_name in tag_names:
+                    tag_data[tag_name].remove(file_name)
+                    tag_data[tag_name].append(new_file_name)
 
-                    json.dump(tag_data, fpt)
+                update_json(tag_data, config)
     else:
         raise Exception("No such action type: {action_type}")
