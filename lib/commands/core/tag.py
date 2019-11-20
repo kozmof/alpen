c19e912a3bad924eefc0d2538017d4c0ec1f6ba4
Kozo Oeda <kozo.oeda@gmail.com>
2019-11-20 19:01:13 +0900

    Update

--- lib/commands/core/tag.py
+++ lib/commands/core/tag.py
@@ -3,13 +3,8 @@ import json
 from typing import Optional, List
 from .configure import load_config
 from .dir_ops import get_dir_path
-from .metadata import (init_metadata, 
-                      version_check,
-                      recover_missing_keys,
-                      update_metadata_file,
-                      f2t,
-                      METADATA_FILE,
-                      CURENT_FORMAT_VERSION)
+from .metadata import (update_metadata_file,
+                      f2t)
 from .custom_types import Config
 from .consistency import doc_file_exists
 
@@ -30,22 +25,39 @@ def t2f(tag_name: str, config: Config) -> Optional[List[str]]:
 def add_tag(file_name, tag_name):
     config: Config = load_config()
     if not doc_file_exists(file_name):
-        print(f"{doc_path} does not exist.")
-        return
+        print(f"{doc_path} does not exist.") return
+    else:
+        update_tag_file("ADD_TAG", file_name, config, tag_name=tag_name)
+        update_metadata_file("ADD_TAG", file_name, config, tag_name=tag_name)
 
-    update_tag_file("ADD_TAG", file_name, config, tag_name=tag_name)
-    update_metadata_file("ADD_TAG", file_name, config, tag_name=tag_name)
 
+def remove_tag(file_name, tag_name):
+    config: Config = load_config()
+    if not doc_file_exists(file_name):
+        print(f"{doc_path} does not exist.") return
+    else:
+        update_tag_file("REMOVE_TAG", file_name, config, tag_name=tag_name)
+        update_metadata_file("REMOVE_TAG", file_name, config, tag_name=tag_name)
+        
 
-def arg_check_t(action_type: str):
+def arg_check_t(action_type: str,
+                tag_name: Optional[str], new_tag_name: Optional[str],
+                new_file_name: Optional[str]):
+
     if action_type == "ADD_TAG":
-        if tag_name is None and new_file_name:
+        if tag_name is None and new_tag_name and new_file_name:
             raise Exception("Pass only tag_name")
+
     elif action_type == "REMOVE_TAG":
-        if tag_name is None and new_file_name:
+        if tag_name is None and new_tag_name and new_file_name:
             raise Exception("Pass only tag_name")
+
+    elif action_type == "RENAME_TAG":
+        if tan_name is None and new_tag_name is None and new_file_name:
+            raise Exception("Pass only tag_name and new_tag_name")
+
     elif action_type == "RENAME_FILE":
-        if new_file_name is None and tag_name:
+        if new_file_name is None and new_tag_name and tag_name:
             raise Exception("Pass only new_file_name")
     else:
         raise Exception(f"No such action type: {action_type}")
@@ -70,8 +82,15 @@ def dump_tag_json(tag_data, config: Config):
             json.dump(tag_data, fpt)
 
 
-def update_tag_file(action_type: str, file_name: str, config: Config, tag_name: Optional[str] = None, new_file_name: Optional[str] = None):
-    arg_check_t(action_type)
+def update_tag_file(action_type: str, file_name: str, config: Config,
+                    tag_name: Optional[str] = None, new_tag_name: Optional[str] = None,
+                    new_file_name: Optional[str] = None):
+
+    arg_check_t(action_type=action_type,
+                tag_name=tag_name,
+                new_tag_name=new_tag_name,
+                new_file_name=new_file_name)
+
     if action_type == "ADD_TAG":
         tag_data = load_tag_data(config)
         if tag_data:
@@ -89,11 +108,17 @@ def update_tag_file(action_type: str, file_name: str, config: Config, tag_name: 
 
     elif action_type == "REMOVE_TAG":
         tag_data = load_tag_data(config)
-        if tag_data:
-            if tag_name in tag_data:
-                if file_name in tag_data[tag_name]:
-                    tag_data[tag_name].remove(file_name)
-                    dump_tag_json(tag_data, config)
+        if tag_data and tag_name in tag_data:
+            if file_name in tag_data[tag_name]:
+                tag_data[tag_name].remove(file_name)
+                dump_tag_json(tag_data, config)
+
+    elif action_type == "RENAME_TAG":
+        tag_data = load_tag_data(config)
+        if tag_data and tag_name in tag_data:
+            tag_data[new_tag_name] = tag_data[tag_name]
+            del tag_data[tag_name]
+            dump_tag_json(tag_data, config)
 
     elif action_type == "RENAME_FILE":
         tag_names = f2t(file_name, config)
