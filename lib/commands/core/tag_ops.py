c11669897a6fd158d2a574a466e0b2cc81ea3bb5
Kozo Oeda <kozo.oeda@gmail.com>
2019-11-20 21:30:35 +0900

    Fix

--- lib/commands/core/tag_ops.py
+++ lib/commands/core/tag_ops.py
@@ -8,7 +8,8 @@ from .custom_types import Config
 def add_tag(file_name, tag_name):
     config: Config = load_config()
     if not doc_file_exists(file_name):
-        print(f"{doc_path} does not exist.") return
+        print(f"{doc_path} does not exist.")
+        return
     else:
         update_tag_file("ADD_TAG", file_name, config, tag_name=tag_name)
         update_metadata_file("ADD_TAG", file_name, config, tag_name=tag_name)
@@ -17,7 +18,8 @@ def add_tag(file_name, tag_name):
 def remove_tag(file_name, tag_name):
     config: Config = load_config()
     if not doc_file_exists(file_name):
-        print(f"{doc_path} does not exist.") return
+        print(f"{doc_path} does not exist.")
+        return
     else:
         update_tag_file("REMOVE_TAG", file_name, config, tag_name=tag_name)
         update_metadata_file("REMOVE_TAG", file_name, config, tag_name=tag_name)
