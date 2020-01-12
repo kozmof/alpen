c6fa09c39adcd0e0efedcb6c2bbb635001eda9fa
Kozo Oeda <kozo.oeda@gmail.com>
2020-01-12 14:49:43 +0900

    Update

--- viewer/src/index.tsx
+++ viewer/src/index.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
-
 const ReactMarkdown = require("react-markdown");
-const p = require("../package.json");
-console.log(p)
+
+const setUp = () => {
+  const build_config = require("../package.json");
+  const markdown_content = require("../dummy-mark.md");
+  console.log(build_config)
+  console.log(markdown_content)
+  return markdown_content 
+}
+
+const markdown_content = setUp();
 
 ReactDom.render(
   <div>
+    <ReactMarkdown source={markdown_content["default"]} />
   </div>,
   document.getElementById("demo")
 );
