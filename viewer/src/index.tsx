a76b48f58a21cfe58df441704cd78e538006ad81
Kozo Oeda <kozo.oeda@gmail.com>
2020-01-12 14:00:56 +0900

    Move files

---
+++ viewer/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+
+const ReactMarkdown = require("react-markdown");
+const p = require("../package.json");
+console.log(p)
+
+ReactDom.render(
+  <div>
+  </div>,
+  document.getElementById("demo")
+);
