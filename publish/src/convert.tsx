e38ddc0fb26bc2e05b235da50c77419567aa7cbe
Kozo Oeda <kozo.oeda@gmail.com>
2020-05-28 08:41:59 +0900

    Redesign payload formats

--- publish/src/convert.tsx
+++ publish/src/convert.tsx
@@ -1,24 +1,39 @@
 type Hash = string;
 
-type JSONPayload = {
-  titles: {
-    [title: string]: Hash;
+/**
+ * title/doc-1
+ *      /doc-2
+ * tag/tag-1
+ *    /tag-2
+ * domain/domain-1
+ *       /domain-2
+ */
+
+type PayloadTitle = {
+  [hash: string]: {
+    title: string;
+    tag: string[];
+    domain: string[];
+    publishDate: string;
+    reviseDate: string;
+    relPath: string;
   }
-  pages: {
-    [Hash: string]: {
-      fileName: string;
-      doc: string;
-      history: Array<{diff: string; timestmap: string;}>;
-      tag: string[];
-      domain: string[];
-      publishDate: string;
-      reviseDate: string;
-    }
-  }
-  tags: {
+}
+
+type PayloadPage = {
+  fileName: string;
+  doc: string;
+  history: Array<{diff: string; timestmap: string;}>;
+  tag: string[];
+  domain: string[];
+  publishDate: string;
+  reviseDate: string;
+}
+
+type PayloadTag = {
     [tagName: string]: Hash[];
   }
-  domain: {
+
+type PayloadDomain = {
     [domainName: string]: Hash[];
-  }
 }
