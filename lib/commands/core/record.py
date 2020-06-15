72e9b98a2e51288dcbf7750a20cd2d0e7df9ee61
Kozo Oeda <kozo.oeda@gmail.com>
2020-06-16 01:15:41 +0900

    Add a document

--- lib/commands/core/record.py
+++ lib/commands/core/record.py
@@ -1,3 +1,6 @@
+"""Recored user activities
+"""
+
 import os
 from typing import Optional, List
 from datetime import datetime 
@@ -6,25 +9,43 @@ from lib.commands.core.configure import load_config
 from lib.commands.core.dir_ops import get_dir_path
 
 
-def record_edited_file(file_name):
+def record_edited_file(file_name: str) -> None:
+    """Recored an edited file 
+
+    Args:
+        file_name (str): Edited file name
+    """
     config: Config = load_config()
     root_path: str = config["root_path"]
+
     with open(f"{root_path}/.edited_file_record", "a") as f:
         time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"{file_name} {time_stamp}\n")
 
 
-def read_edited_file_record(active_sign="A", not_active_sign="N") -> Optional[str]:
+def read_edited_file_record(active_sign: str="A", not_active_sign: str="N") -> Optional[str]:
+    """Read records of edited files
+
+    Args:
+        active_sign (str, optional): A sign of file exists. Defaults to "A".
+        not_active_sign (str, optional): A sign of file does not exist. Defaults to "N".
+
+    Returns:
+        Optional[str]: A history of edited files
+    """
     config: Config = load_config()
     root_path: str = config["root_path"]
     record_path = f"{root_path}/.edited_file_record"
+
     if os.path.isfile(record_path):
         with open(record_path) as f:
             result = ""
             records: List[str] = f.readlines()
+
             for record in reversed(records):
                 file_name = "".join(record.split(" ")[:-2])
                 full_path = f"{get_dir_path('DOCUMENT', config)}/{file_name}"
+
                 if os.path.isfile(full_path):
                     result += f"[{active_sign}] {record}"
                 else:
