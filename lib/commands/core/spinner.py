34b8e2346a90b840dc1947290927c356e855751a
Kozo Oeda <kozo.oeda@gmail.com>
2020-06-15 18:32:48 +0900

    Add a document

--- lib/commands/core/spinner.py
+++ lib/commands/core/spinner.py
@@ -1,23 +1,31 @@
-# coding: utf-8
+"""Threading spinner
+"""
 import os
 import time
 import threading
 import sys, itertools
 import subprocess
+from __future__ import annotations
 
-# Based on https://stackoverflow.com/questions/4995733/how-to-create-a-spinning-command-line-cursor
 
 class Spinner:
-    def __init__(self, statement=""):
+    # Based on https://stackoverflow.com/questions/4995733/how-to-create-a-spinning-command-line-cursor
+    def __init__(self, statement: str="", delay=0.03) -> None:
+        """Use with statement when initializing
+
+        Args:
+            statement (str, optional): A text of the right side of the spinner. Defaults to "".
+            delay (str, optional): Adjust a motion speed
+        """
         self.run = False
-        # self.motions = "⣾⣽⣻⢿⡿⣟⣯⣷"
-        # self.motions = "▖ ▘ ▝ ▗"
         self.motions = "|/_\\_"
-        self.delay = 0.03
+        self.delay = delay
         self.statement = statement
         self.thread = None
 
-    def spin(self):
+    def spin(self) -> None:
+        """Process spinning
+        """
         cycles1 = itertools.cycle(self.motions)
         while self.run:
             text = f"{next(cycles1)} {self.statement}"
@@ -26,19 +34,31 @@ class Spinner:
             sys.stdout.flush()
             sys.stdout.write('\b' * len(text))
 
-    def __enter__(self):
+    def __enter__(self) -> Spinner:
+        """Start threading
+
+        Returns:
+            Spinner: Reference of this instance
+        """
         subprocess.run(["clear"])
         self.run = True
         self.thread = threading.Thread(target=self.spin)
         self.thread.start()
         return self
 
-    def __exit__(self, exception, value, tb):
+    def __exit__(self, exception, value, tb) -> None:
+        """Clear buffer and a terminal
+        """
         self.run = False
         sys.stdout.flush()
         subprocess.run(["clear"])
 
-    def change(self, statement):
+    def change(self, statement: str) -> None:
+        """Change a text to show
+
+        Args:
+            statement (str): text to show in the right side of the spinner
+        """
         self.statement = statement
         sys.stdout.flush()
         subprocess.run(["clear"])
