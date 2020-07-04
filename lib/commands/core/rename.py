436c6c8eaeb5bfe6baa82d79713b5d3bc1bdc5b3
Kozo Oeda <kozo.oeda@gmail.com>
2020-07-04 09:09:30 +0900

    Add a document

--- lib/commands/core/rename.py
+++ lib/commands/core/rename.py
@@ -1,3 +1,5 @@
+"""Renaming Utilities
+"""
 import re
 from typing import Tuple, Optional
 
@@ -7,7 +9,15 @@ from lib.commands.core.metadata import update_metadata_file
 from lib.commands.core.configure import load_config
 from lib.commands.core.custom_types import Config
 
-def arg_to_names(arg) -> Tuple[Optional[str], Optional[str]]:
+def arg_to_names(arg: str) -> Tuple[Optional[str], Optional[str]]:
+    """Parse arguments and get an original name and a new name
+
+    Args:
+        arg (str): rename <rename_from> <rename_to>
+
+    Returns:
+        Tuple[Optional[str], Optional[str]]: An original name and a new name
+    """
     result_1 = list(re.findall("\'", arg))
     result_2 = list(re.findall('\"', arg))
     original_name = None
@@ -39,7 +49,14 @@ def arg_to_names(arg) -> Tuple[Optional[str], Optional[str]]:
     return original_name, new_name
 
 
-def apply_rename(file_name, new_file_name, config: Config):
+def apply_rename(file_name: str, new_file_name: str, config: Config) -> None:
+    """Rename a file and change related data
+
+    Args:
+        file_name (str): A file name
+        new_file_name (str): A new file name
+        config (Config): Config data
+    """
     update_tag_file("RENAME_FILE", config, file_name=file_name, new_file_name=new_file_name)
     update_domain_file("RENAME_FILE", config, file_name=file_name, new_file_name=new_file_name)
     update_metadata_file("RENAME_FILE", config, file_name=file_name, new_file_name=new_file_name)
