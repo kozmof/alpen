93745839cedc8baf758b5850a4528603ff1e36fc
Kozo Oeda <kozo.oeda@gmail.com>
2020-07-04 16:10:40 +0900

    Add a document

--- lib/commands/core/domain_ops.py
+++ lib/commands/core/domain_ops.py
@@ -1,3 +1,5 @@
+"""Domain Operations
+"""
 from lib.commands.core.configure import load_config
 from lib.commands.core.domain import manipulate_domain_data
 from lib.commands.core.metadata import manipulate_metadata
@@ -5,7 +7,13 @@ from lib.commands.core.consistency import doc_file_exists
 from lib.commands.core.custom_types import Config
 
 
-def add_domain(file_name, domain_name):
+def add_domain(file_name: str, domain_name: str) -> None:
+    """Add a domain
+
+    Args:
+        file_name (str): A file name to add a domain
+        domain_name (str): A domain name
+    """
     config: Config = load_config()
     if not doc_file_exists(file_name, config):
         print(f"{file_name} does not exist.")
@@ -16,7 +24,13 @@ def add_domain(file_name, domain_name):
         print(f"{domain_name} added to {file_name}")
 
 
-def remove_domain(file_name, domain_name):
+def remove_domain(file_name: str, domain_name: str) -> None:
+    """Remove a domain
+
+    Args:
+        file_name (str): A file to remove a domain
+        domain_name (str): A domain name
+    """
     config: Config = load_config()
     if not doc_file_exists(file_name, config):
         print(f"{file_name} does not exist.")
@@ -27,20 +41,31 @@ def remove_domain(file_name, domain_name):
         print(f"{domain_name} removed from {file_name}")
 
 
-def rename_domain(domain_name, new_domain_name):
+def rename_domain(domain_name: str, new_domain_name: str) -> None:
+    """Rename a domain
+
+    Args:
+        domain_name (str): A domain name to be renamed
+        new_domain_name (str): A new domain name
+    """
     config: Config = load_config()
     manipulate_domain_data("RENAME_DOMAIN", config, domain_name=domain_name, new_domain_name=new_domain_name)
     manipulate_metadata("RENAME_DOMAIN", config, domain_name=domain_name, new_domain_name=new_domain_name)
     print(f"{domain_name} renamed to {new_domain_name}")
 
 
-def search_domain(domain_name):
+def search_domain(domain_name: str):
+    """Search a domain name and print related file names
+
+    Args:
+        domain_name (str): A domain to search
+    """
     config: Config = load_config()
     manipulate_domain_data("SEARCH_DOMAIN", config, domain_name=domain_name)
 
 
 def show_all():
+    """Show all domain data
+    """
     config: Config = load_config()
     manipulate_domain_data("SHOW_ALL", config)
-
-    
